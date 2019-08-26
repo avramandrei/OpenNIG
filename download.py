6f73b3dc9220c38b99ec74df62809e0f27abdda7
avramandrei <avram.andreimarius@gmail.com>
2019-08-26 17:28:16 +0300

    Added .gitignore file. Added download file with mnist option

---
+++ download.py
@@ -0,0 +1,16 @@
+import argparse
+import src.preparation.prepare_mnist as prepare_mnist
+import src.preprocess.process_mnist as process_mnist
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", type=str)
+    args = parser.parse_args()
+
+    if args.dataset == "mnist":
+        mnist_raw_data_path = os.path.join("data", "mnist", "raw")
+        mnist_processed_data_path = os.path.join("data", "mnist", "processed")
+
+        prepare_mnist.prepare_data(mnist_raw_data_path)
+        process_mnist.process_data(mnist_raw_data_path, mnist_processed_data_path)
