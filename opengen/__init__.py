cc3bbf235fa2749b1b323290e8aa9f17dba57893
Avram Andrei Marius <avramandrei9666@gmail.com>
2019-08-28 19:32:39 +0300

    Refactored code

--- opengen/__init__.py
+++ opengen/__init__.py
@@ -1,1 +1,6 @@
 name="opengen"
+
+from .models import *
+from .preparation import *
+from .preprocess import *
+from .util import *
--- setup.py
+++ setup.py
@@ -5,19 +5,29 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="opengen-tf",
-    version="0.1.0",
+    version="0.1.5",
     author="Andrei-Marius Avram",
     author_email="avram.andreimarius@gmail.com",
     description="OpenGen is a toolkit that offers an easy interface to generative models.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/avramandrei/OpenGen",
+	keywords="generative-models tensorflow generative-adversarial-network gan variational-autoencoder vae",
+	license="MIT",
+	install_requires=[
+		"tensorflow>=2.0.0",
+		"Pillow"
+	],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+		"Intended Audience :: Developers",
+		"Intended Audience :: Information Technology",
+		"Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+		"Topic :: Scientific/Engineering :: Visualization"
     ],
 )
