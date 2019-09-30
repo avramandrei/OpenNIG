1f65e255f32099a8e126c5dc09ab64a2f3e2dd86
avramandrei <avram.andreimarius@gmail.com>
2019-09-30 11:56:06 +0300

    Added DCVAEMedium cifar10 train samples

--- process.py
+++ process.py
@@ -12,7 +12,7 @@ def load_images_from_path(path, shape, flip_left_right):
         height = int(shape.split(",")[1][:-1])
 
     for filename in os.listdir(path):
-        if "cat" in filename:
+        if "bird" in filename:
             img = Image.open(os.path.join(path, filename))
 
             if shape is not None:
