79a330c8b611bb1512f26c683c7620aeb2f7f6d4
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 10:30:23 +0000

    Adding tarfile member sanitization to extractall()

--- opennng/preparation/prepare.py
+++ opennng/preparation/prepare.py
@@ -18,7 +18,26 @@ def prepare_mnist():
 
     print("Extracting `mnist` data...")
     with tarfile.open(temp_data_path, "r:gz") as tar:
-        tar.extractall(temp_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, temp_path)
 
     for digit_dir in os.listdir(os.path.join(temp_path, "mnist_png", "training")):
         for image in os.listdir(os.path.join(temp_path, "mnist_png", "training", digit_dir)):
@@ -58,7 +77,26 @@ def prepare_cifar10():
 
     print("Extracting `cifar10` data...")
     with tarfile.open(temp_data_path, "r") as tar:
-        tar.extractall(data_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, data_path)
 
     if os.path.exists(os.path.join(data_path, "train")):
         shutil.rmtree(os.path.join(data_path, "train"))
